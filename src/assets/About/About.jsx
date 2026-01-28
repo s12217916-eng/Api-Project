b6275f5b4178dba63437c0b7d77a0a622e16efbf
Zaid Nouri <s12217916@stu.najah.edu>
2026-01-28 03:36:49 +0200

    add contact componones with responsive

--- src/assets/About/About.jsx
+++ src/assets/About/About.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import styles from './About.module.css'
 export default function About() {
   return (
-    <div className={`${styles['About-color']} About-color text-center p-5`}>
+    <div className={`${styles['About-color']} About-color text-center p-5 mb-5`}>
       <h1 className='text-white fs-1 mt-5 mb-5'>About</h1>
-      <div className='container-fluid w-75 mt-4 mb-5 d-flex align-items-center gap-4 justify-content-start justify-content-center'>
+      <div className='container-fluid w-75 mt-4 mb-5 d-flex align-items-center gap-4 justify-content-start justify-content-center '>
 <p className='text-white fs-5 w-50'>Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
 <p className='text-white fs-5 w-50'>You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p>
 
--- src/assets/Contact/Contact .jsx
+++ src/assets/Contact/Contact .jsx
@@ -1,7 +1,30 @@
 import React from 'react'
-
+import styles from './Contact .module.css'
 export default function Contact () {
   return (
-    <div>Contact </div>
+    <div className="mt-5 w-50 container">
+      
+      <h2 className={`${styles['Contact-color']} text-center fs-1 mt-4`}>Contact Me</h2>
+      <div className=''>
+        <form className='gap-4 text-center'>
+     <div>
+  <label htmlFor="inputPassword5" className="form-label mt-4 text-secondary">Full Name</label>
+  <input type="text" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock" />
+  <label htmlFor="inputPassword5" className="form-label mt-4 text-secondary">Email Addres</label>
+  <input type="email" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock" />
+  <label htmlFor="inputPassword5" className="form-label mt-4 text-secondary">Phone Number</label>
+  <input type="number" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock" />
+  <label htmlFor="inputPassword5" className="form-label mt-4 text-secondary">Message</label>
+  <input type="text" id="inputPassword5" className="form-control mb-5" aria-describedby="passwordHelpBlock" />
+  <button type="button" className="btn btn-primary">Primary</button>
+</div>
+
+        </form>
+      </div>
+
+
+
+
+       </div>
   )
 }
