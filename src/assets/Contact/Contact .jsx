7cb6d99949b8c6b0dad119fe1370dcc1ac7cbabf
Zaid Nouri <s12217916@stu.najah.edu>
2026-01-28 04:02:41 +0200

    add Footer Componens with responsive

--- src/assets/Contact/Contact .jsx
+++ src/assets/Contact/Contact .jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './Contact .module.css'
 export default function Contact () {
   return (
-    <div className="mt-5 w-50 container">
+    <div className="mt-5 mb-5 w-50 container">
       
       <h2 className={`${styles['Contact-color']} text-center fs-1 mt-4`}>Contact Me</h2>
       <div className=''>
@@ -16,7 +16,7 @@ export default function Contact () {
   <input type="number" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock" />
   <label htmlFor="inputPassword5" className="form-label mt-4 text-secondary">Message</label>
   <input type="text" id="inputPassword5" className="form-control mb-5" aria-describedby="passwordHelpBlock" />
-  <button type="button" className="btn btn-primary">Primary</button>
+  <button type="button" className={`btn btn-primary ${styles['btn1']}`}>Primary</button>
 </div>
 
         </form>
--- src/assets/Footer/Footer.jsx
+++ src/assets/Footer/Footer.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
-
+import styles from './Footer.module.css'
 export default function Footer() {
   return (
-    <div>Footer</div>
+    
+    <div className={`${styles['Footer-background']} text-center mt-5 py-4 `}>
+<div className={`${styles['gap-footer']} container gap-5 d-flex justify-content-center  g-5 `}>
+<div className=''>
+
+<p className='fs-2 text-white'>Location</p>
+  <p className='text-white'>2215 John Daniel Drive</p>
+  <p className='text-white'>Clark, MO 65243</p>
+</div>
+<div className=''>
+<p className='fs-2 text-white'>Around the Web</p>
+</div>
+
+<div className=''>
+<p className='fs-2 text-white'>About Freelancer</p>
+  <p className='text-white'>Freelance is a free to use, MIT licensed <br/>Bootstrap theme created by Start Bootstrap .</p>
+</div>
+</div>
+
+    </div>
   )
 }
